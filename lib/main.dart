af8ea16d1367d8b969ddc78868e13312bf03584c
AndreaFalanti <a.falanti@gmail.com>
2020-11-16 12:25:28 +0100

    First prototype of Unity scene integration

---
+++ lib/main.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_unity_widget/flutter_unity_widget.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  static final GlobalKey<ScaffoldState> _scaffoldKey =
+  GlobalKey<ScaffoldState>();
+  UnityWidgetController _unityWidgetController;
+  double _sliderValue = 0.1;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        key: _scaffoldKey,
+        appBar: AppBar(
+          title: const Text('Unity Flutter Demo'),
+        ),
+        body: Card(
+          margin: const EdgeInsets.all(8),
+          clipBehavior: Clip.antiAlias,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          child: Stack(
+            children: <Widget>[
+              UnityWidget(
+                onUnityViewCreated: onUnityCreated,
+                isARScene: true,
+                onUnityMessage: onUnityMessage,
+                onUnitySceneLoaded: onUnitySceneLoaded,
+                fullscreen: false,
+              ),
+              Positioned(
+                bottom: 20,
+                left: 20,
+                right: 20,
+                child: Card(
+                  elevation: 10,
+                  child: Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(top: 20),
+                        child: Text("Rotation speed:"),
+                      ),
+                      Slider(
+                        onChanged: (value) {
+                          setState(() {
+                            _sliderValue = value;
+                          });
+                          setObjPosition(value.toString());
+                        },
+                        value: _sliderValue,
+                        min: 0,
+                        max: 1,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  // Communication from Flutter to Unity
+  void setObjPosition(String speed) {
+    _unityWidgetController.postMessage(
+      'Cube',
+      'setObjPosition',
+      speed,
+    );
+  }
+
+  // Communication from Unity to Flutter
+  void onUnityMessage(controller, message) {
+    print('Received message from unity: ${message.toString()}');
+  }
+
+  // Callback that connects the created controller to the unity controller
+  void onUnityCreated(controller) {
+    this._unityWidgetController = controller;
+  }
+
+  // Communication from Unity when new scene is loaded to Flutter
+  void onUnitySceneLoaded(
+      controller, {
+        int buildIndex,
+        bool isLoaded,
+        bool isValid,
+        String name,
+      }) {
+    print('Received scene loaded from unity: $name');
+    print('Received scene loaded from unity buildIndex: $buildIndex');
+  }
+
+}
---
+++ unity/AR_fondation_test/Assets/Scripts/PlacementIndicator.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class PlacementIndicator : MonoBehaviour
+{
+
+    private ARRaycastManager rayManager;
+    private GameObject indicator;
+    public ObjectSpawner spawner;
+    private bool spawned = false;
+    private GameObject model;
+
+     void Start()
+    {
+        // get the component
+        rayManager = FindObjectOfType<ARRaycastManager>();
+        indicator = transform.GetChild(0).gameObject;
+
+        // hide the placement visual
+        indicator.SetActive(false);
+    }
+
+     void Update()
+    {
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+        rayManager.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), hits, TrackableType.Planes);
+
+        // if we hit AR Plane, Update the postion and rotation
+        if (hits.Count > 0) {
+            transform.position = hits[0].pose.position;
+            transform.rotation = hits[0].pose.rotation;
+
+            if (!indicator.activeInHierarchy && !spawned)
+                indicator.SetActive(true);
+
+            if (Input.touchCount > 0) {
+                if (!spawned) {
+                    spawned = true;
+                    indicator.SetActive(false);
+                    model = spawner.Activate();
+                }
+                else {
+                    model.transform.position = this.gameObject.transform.position;
+                }
+            }
+        }
+    }
+
+}
