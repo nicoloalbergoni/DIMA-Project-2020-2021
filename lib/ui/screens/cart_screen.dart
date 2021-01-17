bdc15678a37566113dc11e24b403db15c9d47ae1
Nicolò Albergoni <albergoni.nicolo@gmail.com>
2021-01-17 17:34:13 +0100

    Refactor cart screen, Fix swipe-right glitch problem
    Now the fetch of the cart items documents is handled by a single FutureBuilder instead than a FutureBuilder for each cart item.

--- lib/ui/screens/cart_screen.dart
+++ lib/ui/screens/cart_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -8,7 +9,6 @@ import 'package:realiteye/redux/actions.dart';
 import 'package:realiteye/redux/app_state.dart';
 import 'package:realiteye/ui/widgets/cart_bottom_sheet.dart';
 import 'package:realiteye/ui/widgets/custom_appbar.dart';
-import 'package:realiteye/ui/widgets/firebase_doc_future_builder.dart';
 import 'package:realiteye/ui/widgets/side_menu.dart';
 import 'package:realiteye/utils/data_service.dart';
 import 'package:realiteye/utils/product_screen_args.dart';
@@ -22,11 +22,31 @@ class CartScreen extends StatefulWidget {
 
 class _CartScreenState extends State<CartScreen> {
   List<TextEditingController> textControllers;
+  Map<String, DocumentSnapshot> documentList;
 
   @override
   void initState() {
     super.initState();
     textControllers = [];
+    documentList = Map<String, DocumentSnapshot>();
+
+    // getDocumentList().then((value) {
+    //   documentList = value;
+    //   print(documentList.length);
+    // });
+
+  }
+
+  Future<Map<String, DocumentSnapshot>> _getDocumentList() async {
+    Map<String, DocumentSnapshot> tempList = Map<String, DocumentSnapshot>();
+    List<CartItem> itemList = StoreProvider.of<AppState>(context, listen: false).state.cartItems;
+
+    for(CartItem i in itemList) {
+      DocumentSnapshot doc = await getProductDocument(i.productId);
+      tempList[i.productId.id] = doc;
+    }
+
+    return tempList;
   }
 
   @override
@@ -60,163 +80,176 @@ class _CartScreenState extends State<CartScreen> {
           );
         },
         builder: (context, viewModel) {
-          return Column(
-            children: [
-              Expanded(
-                child: ListView.separated(
-                  shrinkWrap: true,
-                  // padding used for whole list, not for items
-                  padding: const EdgeInsets.only(top: 6.0),
-                  itemCount: viewModel.cartItems.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    CartItem cartItem = viewModel.cartItems[index];
-                    TextEditingController textQuantityController =
-                        TextEditingController(
-                            text: cartItem.quantity.toString());
-                    textControllers.add(textQuantityController);
-                    return FirebaseDocFutureBuilder(
-                      getProductDocument(cartItem.productId),
-                      (data) {
-                        // TODO: Fix dismissible problem in listview
-                        return Dismissible(
-                          key: UniqueKey(),
-                          direction: DismissDirection.startToEnd,
-                          onDismissed: (_) {
-                            viewModel
-                                .removeFromCartCallback(cartItem.productId);
-                            displaySnackbarWithText(
-                                context, 'Product removed from cart');
-                          },
-                          background: Container(
-                            alignment: Alignment.centerLeft,
-                            padding: EdgeInsets.only(left: 25),
-                            color: Colors.red,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Icon(Icons.delete, color: Colors.white),
-                                Text(
-                                  LocaleKeys.cart_dismissible_delete.tr(),
-                                  style: TextStyle(color: Colors.white),
+          return FutureBuilder(
+              future: _getDocumentList(),
+              builder: (context, data) {
+                if(data.hasData) {
+                  documentList = data.data;
+                  return Column(
+                    children: [
+                      Expanded(
+                        child: ListView.separated(
+                          shrinkWrap: true,
+                          // padding used for whole list, not for items
+                          padding: const EdgeInsets.only(top: 6.0),
+                          itemCount: documentList.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            CartItem cartItem = viewModel.cartItems[index];
+                            Map<String, dynamic> data = documentList[cartItem.productId.id].data();
+                            TextEditingController textQuantityController =
+                            TextEditingController(
+                                text: cartItem.quantity.toString());
+                            textControllers.add(textQuantityController);
+                            return  Dismissible(
+                              key: UniqueKey(),
+                              direction: DismissDirection.startToEnd,
+                              onDismissed: (_) {
+                                documentList.remove(cartItem.productId.id);
+                                viewModel
+                                    .removeFromCartCallback(cartItem.productId);
+                                displaySnackbarWithText(
+                                    context, 'Product removed from cart');
+                              },
+                              background: Container(
+                                alignment: Alignment.centerLeft,
+                                padding: EdgeInsets.only(left: 25),
+                                color: Colors.red,
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Icon(Icons.delete, color: Colors.white),
+                                    Text(
+                                      LocaleKeys.cart_dismissible_delete.tr(),
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                  ],
                                 ),
-                              ],
-                            ),
-                          ),
-                          child: ListTile(
-                            leading: SizedBox(
-                              height: 60,
-                              width: 60,
-                              child: Image.network(
-                                data['thumbnail'],
-                                fit: BoxFit.cover,
-                                loadingBuilder: onImageLoad,
-                                errorBuilder: onImageError,
                               ),
-                            ),
-                            title: Text(data['name']),
-                            onTap: () {
-                              Navigator.pushNamed(context, '/product',
-                                  arguments: ProductScreenArgs(
-                                      cartItem.productId, data));
-                            },
-                            subtitle: Row(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                Text(
-                                    '${LocaleKeys.quantity.tr()}: ${cartItem.quantity}'),
-                                Spacer(),
-                                Container(
-                                  padding: const EdgeInsets.all(0.0),
-                                  height: 36.0,
-                                  width: 40.0,
-                                  child: IconButton(
-                                    icon: Icon(Icons.remove),
-                                    onPressed: () {
-                                      int newQuantity = cartItem.quantity - 1;
-                                      viewModel.changeCartItemQuantityCallback(
-                                          CartItem(
-                                              cartItem.productId, newQuantity));
-                                    },
+                              child: ListTile(
+                                leading: SizedBox(
+                                  height: 60,
+                                  width: 60,
+                                  child: Image.network(
+                                    data['thumbnail'],
+                                    fit: BoxFit.cover,
+                                    loadingBuilder: onImageLoad,
+                                    errorBuilder: onImageError,
                                   ),
                                 ),
-                                Container(
-                                  padding: const EdgeInsets.all(0.0),
-                                  height: 36.0,
-                                  width: 40.0,
-                                  child:  Focus(
-                                      onFocusChange: (focus) {
-                                        if (!focus) {
-                                          int quantity = int.tryParse(
-                                              textQuantityController.text);
-                                          if (quantity == null ||
-                                              quantity < 1 ||
-                                              quantity > 50) {
-                                            displaySnackbarWithText(context,
-                                                "The quantity must be between 1 and 50");
-                                            textQuantityController.text = cartItem.quantity.toString();
-                                          } else {
-                                            viewModel
-                                                .changeCartItemQuantityCallback(
-                                                CartItem(
-                                                    cartItem.productId,
-                                                    quantity));
-                                          }
-                                        }
-                                      },
-                                      child: TextField(
-                                        textAlign: TextAlign.center,
-                                        keyboardType: TextInputType.number,
-                                        controller: textQuantityController,
-                                        onSubmitted: (value) {
-                                          int quantity = int.tryParse(value);
-                                          if (quantity != null) {
-                                            if (quantity < 1 || quantity > 50)
-                                              displaySnackbarWithText(context,
-                                                  "The quantity must be between 1 and 50");
-                                            else
-                                              viewModel
-                                                  .changeCartItemQuantityCallback(
-                                                      CartItem(
-                                                          cartItem.productId,
-                                                          quantity));
-                                          } else {
-                                            displaySnackbarWithText(context,
-                                                "The quantity cannot be null");
-                                            textQuantityController.text =
-                                                cartItem.quantity.toString();
-                                          }
+                                title: Text(data['name']),
+                                onTap: () {
+                                  Navigator.pushNamed(context, '/product',
+                                      arguments: ProductScreenArgs(
+                                          cartItem.productId, data));
+                                },
+                                subtitle: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Text(
+                                        '${LocaleKeys.quantity.tr()}: ${cartItem.quantity}'),
+                                    Spacer(),
+                                    Container(
+                                      padding: const EdgeInsets.all(0.0),
+                                      height: 36.0,
+                                      width: 40.0,
+                                      child: IconButton(
+                                        icon: Icon(Icons.remove),
+                                        onPressed: () {
+                                          int newQuantity = cartItem.quantity - 1;
+                                          viewModel.changeCartItemQuantityCallback(
+                                              CartItem(
+                                                  cartItem.productId, newQuantity));
                                         },
                                       ),
                                     ),
-                                  ),
+                                    Container(
+                                      padding: const EdgeInsets.all(0.0),
+                                      height: 36.0,
+                                      width: 40.0,
+                                      child:  Focus(
+                                        onFocusChange: (focus) {
+                                          if (!focus) {
+                                            int quantity = int.tryParse(
+                                                textQuantityController.text);
+                                            if (quantity == null ||
+                                                quantity < 1 ||
+                                                quantity > 50) {
+                                              displaySnackbarWithText(context,
+                                                  "The quantity must be between 1 and 50");
+                                              textQuantityController.text = cartItem.quantity.toString();
+                                            } else {
+                                              viewModel
+                                                  .changeCartItemQuantityCallback(
+                                                  CartItem(
+                                                      cartItem.productId,
+                                                      quantity));
+                                            }
+                                          }
+                                        },
+                                        child: TextField(
+                                          textAlign: TextAlign.center,
+                                          keyboardType: TextInputType.number,
+                                          controller: textQuantityController,
+                                          onSubmitted: (value) {
+                                            int quantity = int.tryParse(value);
+                                            if (quantity != null) {
+                                              if (quantity < 1 || quantity > 50)
+                                                displaySnackbarWithText(context,
+                                                    "The quantity must be between 1 and 50");
+                                              else
+                                                viewModel
+                                                    .changeCartItemQuantityCallback(
+                                                    CartItem(
+                                                        cartItem.productId,
+                                                        quantity));
+                                            } else {
+                                              displaySnackbarWithText(context,
+                                                  "The quantity cannot be null");
+                                              textQuantityController.text =
+                                                  cartItem.quantity.toString();
+                                            }
+                                          },
+                                        ),
+                                      ),
+                                    ),
 
-                                Container(
-                                  padding: const EdgeInsets.all(0.0),
-                                  height: 36.0,
-                                  width: 40.0,
-                                  child: IconButton(
-                                    icon: Icon(Icons.add),
-                                    onPressed: () {
-                                      int newQuantity = cartItem.quantity + 1;
-                                      viewModel.changeCartItemQuantityCallback(
-                                          CartItem(
-                                              cartItem.productId, newQuantity));
-                                    },
-                                  ),
+                                    Container(
+                                      padding: const EdgeInsets.all(0.0),
+                                      height: 36.0,
+                                      width: 40.0,
+                                      child: IconButton(
+                                        icon: Icon(Icons.add),
+                                        onPressed: () {
+                                          int newQuantity = cartItem.quantity + 1;
+                                          viewModel.changeCartItemQuantityCallback(
+                                              CartItem(
+                                                  cartItem.productId, newQuantity));
+                                        },
+                                      ),
+                                    ),
+                                  ],
                                 ),
-                              ],
-                            ),
-                          ),
-                        );
-                      },
-                    );
-                  },
-                  separatorBuilder: (BuildContext context, int index) =>
-                      const Divider(),
-                ),
-              )
-            ],
-          );
+                              ),
+                            );
+                          },
+                          separatorBuilder: (BuildContext context, int index) =>
+                          const Divider(),
+                        ),
+                      )
+                    ],
+                  );
+                }
+                return Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      CircularProgressIndicator(),
+                      SizedBox(height: 10,),
+                      Text("Loading")
+                    ],
+                  ),
+                );
+          });
         },
       ),
       floatingActionButton: FloatingActionButton.extended(
